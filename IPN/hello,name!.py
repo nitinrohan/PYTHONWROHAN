212f36fa601a57395554980d857b179d64bac0b8
Rohan <nitinrohan56@gmail.com>
2025-04-02 12:34:14 -0500

    its a code to greet someone with hello and their name with , and exclamation marks

---
+++ IPN/hello,name!.py
@@ -0,0 +1,5 @@
+# Write a program that greets the user by printing the word "Hello", a comma, the name of the user and an exclamation mark after it. See the examples below.
+# Warning. Your program's output should strictly match the desired one, character by character. There shouldn't be any space between the name and the exclamation mark. You can use + operator to concatenate two strings. See the lesson for details.
+
+n=input("write a name : ")
+print("Hello"+",",n + "!")
