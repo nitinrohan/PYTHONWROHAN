28f44b3e0cd33cd05076ac7eabcd76196b2fdb49
Rohan <nitinrohan56@gmail.com>
2025-04-02 12:26:37 -0500

    A python code to find the area of any given right angled triangle

---
+++ areaoftriangle.py
@@ -0,0 +1,7 @@
+# Write a program that reads the length of the base and the height of a right-angled triangle and prints the area. Every number is given on a separate line
+# formula is (1/2)b*h
+
+b=int(input())
+h=int(input())
+a=(b*h)/2
+print(a)
